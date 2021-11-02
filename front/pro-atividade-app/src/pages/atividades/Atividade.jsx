1fccffb2edcc6cdb1a37404a957564f18b11eb64
Lucas Antunes <lucasdanielantunes@gmail.com>
2021-11-02 11:20:26 -0300

    Refatorando Atividade

--- front/pro-atividade-app/src/pages/atividades/Atividade.jsx
+++ front/pro-atividade-app/src/pages/atividades/Atividade.jsx
@@ -1,77 +1,145 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import {Button, Modal} from 'react-bootstrap';
+import api from '../../api/atividade';
+import TitlePage from './../../components/TitlePage';
+import AtividadeLista from './AtividadeLista';
+import AtividadeForm from './AtividadeForm';
 
-export default function Atividade(props) {
-    function prioridadeLabel(param) {
-        switch (param) {
-          case "Baixa":
-          case "Normal":
-          case "Alta":
-            return param;
-          default:
-            return "Não definido";
+
+export default function Atividade() {
+    const [showAtividadeModal, setShowAtividadeModal] = useState(false);
+    const [smShowConfirmeModal, setSmShowConfirmeModal] = useState(false);
+    const [atividades, setAtividades] = useState([]);
+    const [atividade, setAtividade] = useState({ id: 0 });
+
+    const handleAtividadeModal = () =>
+        setShowAtividadeModal(!showAtividadeModal);
+
+    const handleConfirmeModal = (id) => {
+        if (id !== 0 && id !== undefined) {
+            const atividade = atividades.filter((atividade) => atividade.id === id);
+            setAtividade(atividade[0]);
+        } else {
+            setAtividade({ id: 0 });
         }
-      }
-    
-      function prioridadeStyle(param, icone) {
-        switch (param) {
-          case "Baixa":
-            return icone ? "smile" : "success";
-          case "Normal":
-            return icone ? "meh" : "warning"; 
-          case "Alta":
-            return icone ? "frown" : "danger"; 
-          default:
-            return "Não definido";
+        setSmShowConfirmeModal(!smShowConfirmeModal);
+    }
+
+
+    const pegaTodasAtividades = async () => {
+        const response = await api.get('atividade');
+        return response.data;
+    }
+
+    const novaAtividade = () => {
+        setAtividade({ id: 0 });
+        handleAtividadeModal();
+    }
+    useEffect(() => {
+        const getAtividades = async () => {
+            const todasAtividades = await pegaTodasAtividades();
+            if (todasAtividades) setAtividades(todasAtividades);
+        };
+        getAtividades();
+    }, [])
+
+    const addAtividade = async (ativ) => {
+        handleAtividadeModal();
+        const response = await api.post('atividade', ativ);
+        setAtividades([...atividades,
+        response.data]);
+    }
+
+    const cancelarAtividade = () => {
+        setAtividade({ id: 0 });
+        handleAtividadeModal();
+    }
+
+    const atualizarAtividade = async (ativ) => {
+        handleAtividadeModal();
+        const response = await api.put(`atividade/${ativ.id}`, ativ);
+        const { id } = response.data;
+
+        setAtividades(
+            atividades.map((item) => (item.id === id ? response.data : item))
+        );
+        setAtividade({ id: 0 });
+    }
+
+    const deletarAtividade = async (id) => {
+        handleConfirmeModal(0);
+        if (await api.delete(`atividade/${id}`)) {
+            const atividadesFiltradas = atividades.filter(atividade => atividade.id !== id);
+            setAtividades([...atividadesFiltradas]);
         }
-      }
+    }
+
+    const pegarAtividade = (id) => {
+        const atividade = atividades.filter((atividade) => atividade.id === id);
+        setAtividade(atividade[0]);
+        handleAtividadeModal();
+    }
+
 
     return (
-      <div
-        
-        className={
-          "card mb-2 shadow-sm border-" + prioridadeStyle(props.ativ.prioridade)
-        }
-      >
-        <div className="card-body">
-          <div className="d-flex justify-content-between">
-            <h5 className="card-title">
-              <span className="badge rounded-pill bg-secondary me-1">
-                {props.ativ.id}
-              </span>
-              - {props.ativ.titulo}
-            </h5>
-            <h6>
-              Prioridade:
-              <span
-                className={" ms-1 text-" + prioridadeStyle(props.ativ.prioridade)}
-              >
-                <i
-                  className={
-                    "me-1 far fa-" + prioridadeStyle(props.ativ.prioridade, true)
-                  }
-                ></i>
-                {prioridadeLabel(props.ativ.prioridade)}
-              </span>
-            </h6>
-          </div>
-          <p className="card-text">{props.ativ.descricao}</p>
-          <div className="d-flex justify-content-end pt-2 m-0 border-top">
-            <button 
-              className="btn btn-outline-primary me-2 btn-sm"
-              onClick={() => props.pegarAtividade(props.ativ.id)}
-              >
-              <i className="fas fa-pen me-2"></i>
-              Editar
-            </button>
-            <button
-              className="btn btn-outline-danger btn-sm"
-              onClick={() => props.handleConfirmeModal(props.ativ.id)}
-            >
-              <i className="fas fa-trash me-2"></i>
-              Deletar
-            </button>
-          </div>
-        </div>
-      </div>
+        <>
+            <TitlePage title={'Atividade ' + (atividade.id !== 0 ? atividade.id : "")}>
+                <Button variant="outline-secondary" onClick={novaAtividade}>
+                    <i className="fas fa-plus"></i>
+                </Button>
+            </TitlePage>
+
+
+            <AtividadeLista
+                atividades={atividades}
+                pegarAtividade={pegarAtividade}
+                handleConfirmeModal={handleConfirmeModal}
+            />
+
+            <Modal show={showAtividadeModal} onHide={handleAtividadeModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Atividade {atividade.id !== 0 ? atividade.id : ""}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <AtividadeForm
+                        addAtividade={addAtividade}
+                        atualizarAtividade={atualizarAtividade}
+                        cancelarAtividade={cancelarAtividade}
+                        ativSelecionada={atividade}
+                        atividades={atividades}
+                    />
+                </Modal.Body>
+            </Modal>
+
+            <Modal size="sm" show={smShowConfirmeModal} onHide={handleConfirmeModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Excluindo Atividade {atividade.id !== 0 ? atividade.id : ""}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Tem certeza que deseja Excluir a Atividade {atividade.id}
+                </Modal.Body>
+                <Modal.Footer className="d-flex justify-content-between">
+                    <button
+                        className="btn btn-outline-success me-2"
+                        onClick={() => deletarAtividade(atividade.id)}
+                    >
+                        <i className="fas fa-check me-2"></i>
+                        Sim
+                    </button>
+                    <button
+                        className="btn btn-danger me-2"
+                        onClick={() => handleConfirmeModal(0)}
+                    >
+                        <i className="fas fa-times me-2"></i>
+                        Não
+                    </button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
+
---
+++ front/pro-atividade-app/src/pages/atividades/AtividadeItem.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+export default function AtividadeItem(props) {
+    function prioridadeLabel(param) {
+        switch (param) {
+          case "Baixa":
+          case "Normal":
+          case "Alta":
+            return param;
+          default:
+            return "Não definido";
+        }
+      }
+    
+      function prioridadeStyle(param, icone) {
+        switch (param) {
+          case "Baixa":
+            return icone ? "smile" : "success";
+          case "Normal":
+            return icone ? "meh" : "warning"; 
+          case "Alta":
+            return icone ? "frown" : "danger"; 
+          default:
+            return "Não definido";
+        }
+      }
+
+    return (
+      <div
+        
+        className={
+          "card mb-2 shadow-sm border-" + prioridadeStyle(props.ativ.prioridade)
+        }
+      >
+        <div className="card-body">
+          <div className="d-flex justify-content-between">
+            <h5 className="card-title">
+              <span className="badge rounded-pill bg-secondary me-1">
+                {props.ativ.id}
+              </span>
+              - {props.ativ.titulo}
+            </h5>
+            <h6>
+              Prioridade:
+              <span
+                className={" ms-1 text-" + prioridadeStyle(props.ativ.prioridade)}
+              >
+                <i
+                  className={
+                    "me-1 far fa-" + prioridadeStyle(props.ativ.prioridade, true)
+                  }
+                ></i>
+                {prioridadeLabel(props.ativ.prioridade)}
+              </span>
+            </h6>
+          </div>
+          <p className="card-text">{props.ativ.descricao}</p>
+          <div className="d-flex justify-content-end pt-2 m-0 border-top">
+            <button 
+              className="btn btn-outline-primary me-2 btn-sm"
+              onClick={() => props.pegarAtividade(props.ativ.id)}
+              >
+              <i className="fas fa-pen me-2"></i>
+              Editar
+            </button>
+            <button
+              className="btn btn-outline-danger btn-sm"
+              onClick={() => props.handleConfirmeModal(props.ativ.id)}
+            >
+              <i className="fas fa-trash me-2"></i>
+              Deletar
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+}
