064e7c14a2e0bc7198750777363401f855e8e898
Lucas Antunes <lucasdanielantunes@gmail.com>
2021-11-02 16:35:31 -0300

    Add React-Router-Dom

--- front/pro-atividade-app/src/App.js
+++ front/pro-atividade-app/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Atividade from "./pages/atividades/Atividade";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
-    <Atividade/>
+    <Router>
+      <Route  path='/atividades' component={Atividade}/>
+      <Route  path='/clientes' component={Cliente}/>
+      <Route  path='/home' component={Home}/>
+    </Router>
+
     </>
   );
 }
+
+const Cliente = () => (
+  <div>
+    <h1>Cliente</h1>
+  </div>
+)
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+)
